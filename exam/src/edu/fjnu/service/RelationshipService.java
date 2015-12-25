e05c530d857ba38367483f3b18bd7b9582fde3e6
vengeancehuang <vengeancehuang@gmail.com>
2015-12-25 13:38:05 +0800

    update relation service's tool

--- exam/src/edu/fjnu/service/RelationshipService.java
+++ exam/src/edu/fjnu/service/RelationshipService.java
@@ -1,8 +1,12 @@
 package edu.fjnu.service;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.rosuda.REngine.Rserve.RserveException;
+
+import edu.fjnu.util.CSVUtil;
 import net.sf.json.JSONObject;
 
 public class RelationshipService 
@@ -66,5 +70,60 @@ public class RelationshipService
 		}else{
 			//把它们连起来
 		}
+	}
+	
+	/**
+	 * 处理为关联的知识列表的数据
+	 * @param courseName
+	 * @param year
+	 * @return
+	 */
+	public static List<String> dealRelationList( String courseName, String year)
+	{
+		RelationService relation = new RelationService();
+		List<String> dealList = new ArrayList<String>();
+		List<List<String>> initial = new ArrayList<List<String>>();
+		initial.addAll(relation.getRelationListForRtool(courseName, year));
+		System.out.println(initial);
+		String temp;
+		for( int i=0; i<initial.size(); i++)
+		{
+			temp = initial.get(i).toString();
+			temp = temp.replace("[", "");
+			temp = temp.replace("]", "");
+			dealList.add(temp);
+		}
+		for( int i1=0; i1<initial.size(); i1++)
+		{
+			System.out.println(initial.get(i1));
+		}
+		for( int i1=0; i1<dealList.size(); i1++)
+		{
+			System.out.println(dealList.get(i1));
+		}
+		return dealList;
+	}
+	
+	/**
+	 * 
+	 * @param courseName
+	 * @param year
+	 */
+	public void createRelationListCSV( String courseName, String year)
+	{
+		CSVUtil csvtool = new CSVUtil();
+		List<String> relationlist = dealRelationList(courseName, year);
+		File file = new File(System.getProperty("user.dir").replace("\\", "/")
+							+"/WebContent/R/temp.csv");
+	    boolean isCSVSuccess = csvtool.exportCsv(file, relationlist);
+	    System.out.println(isCSVSuccess);
+	}
+	
+	
+	public static void main( String[] args) throws RserveException
+	{
+		RelationshipService r = new RelationshipService();
+		r.createRelationListCSV("语文", "三年%");
+	
 	}
 }
