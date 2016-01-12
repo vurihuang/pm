d0e7cf277238a62fe943a3737833af8a7aafc906
vengeancehuang <vengeancehuang@gmail.com>
2016-01-12 19:10:07 +0800

    修改关联分析bug

--- exam/src/edu/fjnu/servlet/RelationServlet.java
+++ exam/src/edu/fjnu/servlet/RelationServlet.java
@@ -28,7 +28,8 @@ import edu.fjnu.util.RelationResult;
 public class RelationServlet extends BaseServlet {
 	private static final long serialVersionUID = 1L;
 	private boolean isStudent;// 判断当前是否为学生登录
-//	private RelationshipService rsh = new RelationshipService();// 用来处理R图数据及调用
+	// private RelationshipService rsh = new RelationshipService();//
+	// 用来处理R图数据及调用
 	private TeacherPaperService tts = new TeacherPaperService();// 得到老师的教学信息
 	private ClusterService cluserService = new ClusterService();// 引入聚类service
 	private StudentPaperService sts = new StudentPaperService();// 为了得到学生所做过试卷的年级列表
@@ -68,73 +69,81 @@ public class RelationServlet extends BaseServlet {
 			String course = request.getParameter("course");
 			// 截取前面三个字符串，得到类似于（三年级）
 			String year = request.getParameter("grade").substring(0, 3);
-			
-			String[] keywordArr = rr.getKeywordArr(year, course);		
+
+			String[] keywordArr = rr.getKeywordArr(year, course);
 			Object[][] stvArr = rr.getStvArr(year, course);
 
 			// 定义两个转化后的二维数组，分别由keywordArr，stvArr转化。
-			String[][] newKeywordArr = new String[keywordArr.length][2];
-			Object[][] newStvArr = new Object[stvArr.length][4];
-			
-			// 存放可以高亮的知识点下标，供stvArr转化使用
-			List<Integer> sourceList = new ArrayList<Integer>();
-			// 存放知识点List
-			List<Cluster> clusterList = new ArrayList<Cluster>();
-			if (isStudent) {// 当前登录为学生
-				String studentID = (String) request.getSession().getAttribute("userID");
-				// 得到该学生所需知识点List
-				clusterList = cluserService.getKeywordListOfStudent(grade, courseName, Integer.parseInt(studentID));
-			}
-			//有关联网数据才能进入
-			if(keywordArr != null && stvArr != null){
-			
-				// 转化keywordArr数组为带高亮参数的二维数组
-				for (int i = 0; i < keywordArr.length; i++) {
-					newKeywordArr[i][0] = keywordArr[i];
-					if (isStudent) {
-						int j;
-						for (j = 0; j < clusterList.size(); j++) {
-							if (changeString(clusterList.get(j).getName()).equals(keywordArr[i])) {
-								newKeywordArr[i][1] = "1";
-								sourceList.add(i);
-								break;
+			String[][] newKeywordArr;
+			Object[][] newStvArr;
+
+			if (keywordArr != null && stvArr != null) {
+
+				// 定义两个转化后的二维数组，分别由keywordArr，stvArr转化。
+				newKeywordArr = new String[keywordArr.length][2];
+				newStvArr = new Object[stvArr.length][4];
+
+				// 存放可以高亮的知识点下标，供stvArr转化使用
+				List<Integer> sourceList = new ArrayList<Integer>();
+				// 存放知识点List
+				List<Cluster> clusterList = new ArrayList<Cluster>();
+				if (isStudent) {// 当前登录为学生
+					String studentID = (String) request.getSession().getAttribute("userID");
+					// 得到该学生所需知识点List
+					clusterList = cluserService.getKeywordListOfStudent(grade, courseName, Integer.parseInt(studentID));
+				}
+				// 有关联网数据才能进入
+				if (keywordArr != null && stvArr != null) {
+
+					// 转化keywordArr数组为带高亮参数的二维数组
+					for (int i = 0; i < keywordArr.length; i++) {
+						newKeywordArr[i][0] = keywordArr[i];
+						if (isStudent) {
+							int j;
+							for (j = 0; j < clusterList.size(); j++) {
+								if (changeString(clusterList.get(j).getName()).equals(keywordArr[i])) {
+									newKeywordArr[i][1] = "1";
+									sourceList.add(i);
+									break;
+								}
 							}
-						}
-						if (j == clusterList.size()) {
+							if (j == clusterList.size()) {
+								newKeywordArr[i][1] = "0";
+							}
+						} else {
 							newKeywordArr[i][1] = "0";
 						}
-					} else {
-						newKeywordArr[i][1] = "0";
 					}
-				}
-				// 转化stvArr数组为带高亮参数的二维数组
-				for (int i = 0; i < stvArr.length; i++) {
-					newStvArr[i][0] = stvArr[i][0];
-					newStvArr[i][1] = stvArr[i][1];
-					newStvArr[i][2] = stvArr[i][2];
-					if (isStudent) {
-						int j;
-						for (j = 0; j < sourceList.size(); j++) {
-							
-							if (sourceList.get(j) == stvArr[i][0] || sourceList.get(j) == stvArr[i][1]) {
-								newStvArr[i][3] = "1";
-								break;
+					// 转化stvArr数组为带高亮参数的二维数组
+					for (int i = 0; i < stvArr.length; i++) {
+						newStvArr[i][0] = stvArr[i][0];
+						newStvArr[i][1] = stvArr[i][1];
+						newStvArr[i][2] = stvArr[i][2];
+						if (isStudent) {
+							int j;
+							for (j = 0; j < sourceList.size(); j++) {
+
+								if (sourceList.get(j) == stvArr[i][0] || sourceList.get(j) == stvArr[i][1]) {
+									newStvArr[i][3] = "1";
+									break;
+								}
 							}
-						}
-						if (j == sourceList.size()) {
+							if (j == sourceList.size()) {
+								newStvArr[i][3] = "0";
+							}
+						} else {
 							newStvArr[i][3] = "0";
 						}
-					} else {
-						newStvArr[i][3] = "0";
 					}
 				}
-			}else{//知识关联返回为空时，给其设定一个假数据。
-				newKeywordArr[0][0] = "没有可关联知识点";
-				newKeywordArr[0][1] = "0";
+			} else {
+				newKeywordArr = new String[1][2];
+				newStvArr = new Object[1][4];
+				newKeywordArr[0][0] = "没有可关联知识点,请重新选择";
 				newStvArr[0][0] = 0;
 				newStvArr[0][1] = 0;
-				newStvArr[0][2] = "0";
-				newStvArr[0][3] = "0";
+				newStvArr[0][2] = 0;
+				newStvArr[0][3] = 0;
 			}
 
 			request.setAttribute("keywordArray", newKeywordArr);
@@ -214,12 +223,12 @@ public class RelationServlet extends BaseServlet {
 		request.setAttribute("img_Graph", GraphPath);
 		request.setAttribute("img_GroupedMatrix", GroupedMatrixPath);
 	}
-	
-	public String changeString(String keyword){
-		if(keyword.contains(" ")){
-			String [] strArray = keyword.split(" ");
+
+	public String changeString(String keyword) {
+		if (keyword.contains(" ")) {
+			String[] strArray = keyword.split(" ");
 			return strArray[1];
-		}else{
+		} else {
 			return keyword;
 		}
 	}
