4cf42a0b76d62c938b6894be9457f8c060923d40
vengeancehuang <vengeancehuang@gmail.com>
2016-01-12 21:31:36 +0800

    更新学生老师试卷分析业务处理代码

--- exam/src/edu/fjnu/service/StudentPaperService.java
+++ exam/src/edu/fjnu/service/StudentPaperService.java
@@ -10,69 +10,84 @@ import edu.fjnu.dao.StudentPaperDao;
 import edu.fjnu.domain.VQuestion;
 import edu.fjnu.domain.VScope;
 import edu.fjnu.domain.VTestMain;
+import edu.fjnu.util.DoubleFormat;
 
 /**
  * 学生端试卷分析的service
+ * 
  * @author 丽标
  *
  */
 public class StudentPaperService {
 
-	private StudentPaperDao std=new StudentPaperDao();
-	private StudentDao studentDao=new StudentDao();
+	private StudentPaperDao std = new StudentPaperDao();
+	private StudentDao studentDao = new StudentDao();
+	private DoubleFormat df = new DoubleFormat();
+
 	/**
 	 * 根据 学科 姓名 年级 得到 学生考卷的list
+	 * 
 	 * @param subject
 	 * @param studentName
 	 * @param gradeName
 	 * @return
 	 */
-	public List<VTestMain> getTestMainsList(String subject,int studentId,String gradeName){
-		
+	public List<VTestMain> getTestMainsList(String subject, int studentId, String gradeName) {
+
 		return std.findTestMainId(subject, studentId, gradeName);
 	}
+
 	/**
 	 * 输入学生id 查询学生做过试卷的所有年级
+	 * 
 	 * @param studentId
 	 * @return
 	 */
-	public List<VScope> getStudentGradeList(int studentId){
+	public List<VScope> getStudentGradeList(int studentId) {
 		return studentDao.getStudentGrade(studentId);
 	}
 
 	/**
 	 * 传入试卷的id 得到 这张试卷所有的题目表vQuestion中的subject(题目内容) ,successnum(答对人数),num（总人数
-	 * ），rate（错误率） stustatus（学生对错）
-	 * 对应前端的表格
+	 * ），rate（错误率） stustatus（学生对错） 对应前端的表格
+	 * 
 	 * @param testMainId
-	 * @return  List<VQuestion>
+	 * @return List<VQuestion>
 	 */
-	public List<VQuestion> getVQuestionList(int testMainId)
-	{
-		return std.searchQuestionList(testMainId);
+	public List<VQuestion> getVQuestionList(int testMainId) {
+		List<VQuestion> tmpList = std.searchQuestionList(testMainId);
+
+		for (int i = 0; i < tmpList.size(); i++) {
+			tmpList.get(i).setRate(df.getDoubleFormat(tmpList.get(i).getRate() * 100));
+		}
+		
+		return tmpList;
 	}
-	
+
 	/**
-	 * 传入 试卷测试范围（scope）得到 知识点（keyword） 
-	 * 知识点的错误率（wrong）
+	 * 传入 试卷测试范围（scope）得到 知识点（keyword） 知识点的错误率（wrong）
+	 * 
 	 * @param scope
 	 * @return List<VQuestion>
 	 */
-	public List<VQuestion> getKeywordAndWrongByScope(String scope){
-		return std.searchWrongRate(scope);
+	public List<VQuestion> getKeywordAndWrongByScope(String scope) {
+		List<VQuestion> tmpList = std.searchWrongRate(scope);
+
+		for (int i = 0; i < tmpList.size(); i++) {
+			tmpList.get(i).setWrong(df.getDoubleFormat(tmpList.get(i).getWrong()));
+		}
+		
+		return tmpList;
 	}
-	
+
 	/**
 	 * 输入试卷id 得到改试卷的scope（测试范围）
+	 * 
 	 * @param testId
 	 * @return
 	 */
-	public String getScope(int testId){
+	public String getScope(int testId) {
 		return std.getScope(testId);
 	}
-	
 
-	
-	
-	 
 }
--- exam/src/edu/fjnu/service/TeacherPaperService.java
+++ exam/src/edu/fjnu/service/TeacherPaperService.java
@@ -14,44 +14,47 @@ import edu.fjnu.domain.VQuestion;
 import edu.fjnu.domain.VScope;
 import edu.fjnu.domain.VTeaacherStudent;
 import edu.fjnu.domain.VTestMain;
+import edu.fjnu.util.DoubleFormat;
 
 /**
  * 教师端考卷分析模块的service
+ * 
  * @author Administrator
  *
  */
 public class TeacherPaperService {
-	private StudentPaperDao studentTestDao=new StudentPaperDao();
-	private TeacherPaperDao teacherTestDao= new TeacherPaperDao();
+	private StudentPaperDao studentTestDao = new StudentPaperDao();
+	private TeacherPaperDao teacherTestDao = new TeacherPaperDao();
 	private QuestionDao questionDao = new QuestionDao();
-	
+	private DoubleFormat df = new DoubleFormat();
+
 	/**
 	 * 通过教师id得到年级列表
+	 * 
 	 * @param teacherId
 	 * @return
 	 */
-	public List<String> getGradeList(int teacherId)
-	{
-		List<String> gradeList=new ArrayList<String>();
-		List<VScope> listScopes=teacherTestDao.searchGradeByTeacherId(teacherId);
-		for(VScope vScope:listScopes){
+	public List<String> getGradeList(int teacherId) {
+		List<String> gradeList = new ArrayList<String>();
+		List<VScope> listScopes = teacherTestDao.searchGradeByTeacherId(teacherId);
+		for (VScope vScope : listScopes) {
 			gradeList.add(vScope.getName());
 		}
 		return gradeList;
 	}
-	
-	
+
 	/**
 	 * 查询某个老师教的这些学生id限定某个年级里的某个科目的学生ID
+	 * 
 	 * @param teacherId
 	 * @param courseName
 	 * @param grade
-	 * @return stuIdList 
+	 * @return stuIdList
 	 */
-	public List<Integer> getStudentId(int teacherId,String courseName,String grade){
-		List<Integer> stuIdList=new ArrayList<Integer>();
-		List<VTeaacherStudent> stuList=teacherTestDao.searchStuId(teacherId, courseName, grade);
-		for (VTeaacherStudent v:stuList) {
+	public List<Integer> getStudentId(int teacherId, String courseName, String grade) {
+		List<Integer> stuIdList = new ArrayList<Integer>();
+		List<VTeaacherStudent> stuList = teacherTestDao.searchStuId(teacherId, courseName, grade);
+		for (VTeaacherStudent v : stuList) {
 			stuIdList.add(v.getStudents_memberId());
 		}
 		return stuIdList;
@@ -59,54 +62,62 @@ public class TeacherPaperService {
 
 	/**
 	 * 传入一个学生列表 得到 所有学生的考卷
-	 * @param subject    科目
-	 * @param stuIdList 学生列表
-	 * @param gradeName 年级名字
-	 * @return vList  试卷列表
+	 * 
+	 * @param subject
+	 *            科目
+	 * @param stuIdList
+	 *            学生列表
+	 * @param gradeName
+	 *            年级名字
+	 * @return vList 试卷列表
 	 */
-	public List<VTestMain> getTestMainsList(String subject,List<Integer> stuIdList,String gradeName){
-		
-			List<VTestMain> vList=new ArrayList<VTestMain>();
-			for(int s:stuIdList){
-				for(VTestMain v:studentTestDao.findTestMainId(subject, s, gradeName)){
-					vList.add(v);
-				}
+	public List<VTestMain> getTestMainsList(String subject, List<Integer> stuIdList, String gradeName) {
+
+		List<VTestMain> vList = new ArrayList<VTestMain>();
+		for (int s : stuIdList) {
+			for (VTestMain v : studentTestDao.findTestMainId(subject, s, gradeName)) {
+				vList.add(v);
 			}
-			
-			return vList;
 		}
-	
-	
+
+		return vList;
+	}
+
 	/**
-	 * 输入试卷id 得到 类VQuestion 的知识点keyword 和wrong 错误率 
+	 * 输入试卷id 得到 类VQuestion 的知识点keyword 和wrong 错误率
+	 * 
 	 * @param testMainId
-	 * @return  返回一个List<VQuestion> 
+	 * @return 返回一个List<VQuestion>
 	 */
-	
-	public List<VQuestion> getWrongRate(int testMainId)
-	{
-		String scope=studentTestDao.getScope(testMainId);
+
+	public List<VQuestion> getWrongRate(int testMainId) {
+		String scope = studentTestDao.getScope(testMainId);
 		return studentTestDao.searchWrongRate(scope);
 	}
-	
+
 	/**
-	 * 输入试卷id 得到 这张试卷所有的题目表中的subject（题目） ,successnum（做对人数）,num（总人数）
-	 * ，rate（错误率） stustatus（学生对错） 这些属性都在类VQuestion里面
+	 * 输入试卷id 得到 这张试卷所有的题目表中的subject（题目） ,successnum（做对人数）,num（总人数） ，rate（错误率）
+	 * stustatus（学生对错） 这些属性都在类VQuestion里面
 	 * 
 	 * @param testMainId
 	 * @return VQuestion的一个list
 	 */
-	public List<VQuestion> getQuestionList(int testMainId)
-	{
-		return studentTestDao.searchQuestionList(testMainId);
+	public List<VQuestion> getQuestionList(int testMainId) {
+		List<VQuestion> tmpList = studentTestDao.searchQuestionList(testMainId);
+		
+		for(int i=0; i<tmpList.size(); i++){
+			tmpList.get(i).setRate(df.getDoubleFormat(tmpList.get(i).getRate()*100));
+		}
+		return tmpList;
 	}
-	
+
 	/**
 	 * 根据指定ID得到试卷难度
+	 * 
 	 * @return
 	 * @throws SQLException
 	 */
-	public VQuestion getHardRateById(int testID) throws SQLException{
+	public VQuestion getHardRateById(int testID) throws SQLException {
 		return questionDao.getHard(testID);
 	}
 }
