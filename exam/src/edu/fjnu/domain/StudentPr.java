15412e4acc1d9d8f54572481476952bc90b0462d
vengeancehuang <vengeancehuang@gmail.com>
2016-01-13 18:59:56 +0800

    添加studentPr-domain-toString

--- exam/src/edu/fjnu/domain/StudentPr.java
+++ exam/src/edu/fjnu/domain/StudentPr.java
@@ -5,39 +5,51 @@ package edu.fjnu.domain;
 
 /**
  * 学生pr值的dao 对应excel表
+ * 
  * @author Administrator
  *
  */
 public class StudentPr {
-	private String name;   //学生名字
-	private String classyear;  //年级
-	private double avgPR;   //某个年级平均pr值
-	private String coach;  //教师id
+	private String name; // 学生名字
+	private String classyear; // 年级
+	private double avgPR; // 某个年级平均pr值
+	private String coach; // 教师id
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getClassyear() {
 		return classyear;
 	}
+
 	public void setClassyear(String classyear) {
 		this.classyear = classyear;
 	}
+
 	public double getAvgPR() {
 		return avgPR;
 	}
+
 	public void setAvgPR(double avgPR) {
 		this.avgPR = avgPR;
 	}
+
 	public String getCoach() {
 		return coach;
 	}
+
 	public void setCoach(String coach) {
 		this.coach = coach;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "StudentPr [name=" + name + ", classyear=" + classyear + ", avgPR=" + avgPR + ", coach=" + coach + "]";
+	}
 
 }
